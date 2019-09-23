a009b8e7966aaa56a8f64a86d584861f5cad4a6c
Maciej Cąderek <maciej.caderek@gmail.com>
2019-09-23 03:06:52 +0200

    Multimethod package - added proper documentation

--- packages/multimethod/src/fromMulti.ts
+++ packages/multimethod/src/fromMulti.ts
@@ -1,7 +1,43 @@
 import pipe from '@arrows/composition/pipe'
-import { Method, Multi } from './internal/types'
+import { Method, Multimethod } from './internal/types'
 
-const fromMulti = (...methods: Method[]) => (multimethod: Multi) => {
+/**
+ * Allows to create new multimethods from existing ones in a simple way.
+ *
+ * It works like functional `pipe`, but with additional checks,
+ * specialized error messages and type checking.
+ *
+ * @param {...Method} [methods] Arbitrary number of partially applied methods
+ * @param {Multimethod} multimethod Multimethod on which you want to base new multimethod
+ * @returns {Multimethod} New multimethod (the base one is unchanged)
+ *
+ *
+ * @example <caption>Interface:</caption>
+ * (method1, method2?, ..., methodN?) => (multimethod) => new_multimethod
+ *
+ * @example <caption>Create new multimethod using existing one as a base:</caption>
+ *
+ * const add = multi(
+ *   (a, b) => [(typeof a), (typeof b)],
+ *   method(['number', 'number'], (a, b) => a + b),
+ *   method(['string', 'string'], (a, b) => `${a}${b}`),
+ * )
+ *
+ * const extendedAdd = fromMulti(
+ *   method(['bigint', 'bigint'], (a, b) => a + b),
+ *   method(['number', 'bigint'], (a, b) => BigInt(a) + b),
+ *   method(['bigint', 'number'], (a, b) => a + BigInt(b)),
+ * )(add)
+ *
+ * extendedAdd(1, 2) // -> 3
+ * extendedAdd('foo', 'bar') // -> 'foobar'
+ * extendedAdd(2n, 3n) // -> 5n
+ * extendedAdd(5, 5n) // -> 10n
+ * extendedAdd(9n, 2) // -> 11n
+ *
+ * @see multi, method
+ */
+const fromMulti = (...methods: Method[]) => (multimethod: Multimethod) => {
   return pipe(...methods)(multimethod)
 }
 
--- packages/multimethod/src/method.ts
+++ packages/multimethod/src/method.ts
@@ -6,13 +6,93 @@ import {
 } from './internal/multimethod'
 import { Method, MethodEntries } from './internal/types'
 
-const method: Method = (...args) => {
+/**
+ * Adds method to a multimethod
+ *
+ * @param {any} [caseValue] The value to which the result of dispatch function is matched (if function, then is executed with output of dispatch function)
+ * @param {any} correspondingValue The value that function should return on matching case (if function, then is executed with input arguments)
+ * @param {Multimethod} multimethod Multimethod on which you want to base new multimethod
+ * @returns {Multimethod} New multimethod (the base one is unchanged)
+ *
+ * @example <caption>Interface:</caption>
+ * (caseValue?, correspondingValue) => (multimethod) => new_multimethod
+ *
+ *
+ * @example <caption>Default method as function:</caption>
+ *
+ * const sayHello = multi((user) => user.lang)
+ *
+ * const sayHelloWithDefault = method((user) => `Hello ${usr.name}!`)(sayHello)
+ *
+ * sayHelloWithDefault({ name: 'Alejandro', lang: 'es' }) // -> 'Hello Alejandro!'
+ *
+ *
+ * @example <caption>Default method as other value:</caption>
+ *
+ * const sayHello = multi(() => user.lang)
+ *
+ * const sayHelloWithDefault = method('Hello!')(sayHello)
+ *
+ * sayHelloWithDefault({ name: 'Alejandro', lang: 'es' }) // -> 'Hello!'
+ *
+ *
+ * @example <caption>Case method with caseValue as ordinary value and correspondingValue as a function:</caption>
+ *
+ * const add = multi((a, b) => [(typeof a), (typeof b)])
+ *
+ * const extendedAdd = method(['number', 'number'], (a, b) => a + b)(add)
+ *
+ * extendedAdd(1, 2) // -> 3
+ *
+ *
+ * @example <caption>Case method with caseValue and correspondingValue as ordinary values:</caption>
+ *
+ * const getHexColor = multi() // Uses identity function (x => x) as default dispatch
+ *
+ * const extendedGetHexColor = method('red', '#FF0000')(getHexColor)
+ *
+ * extendedGetHexColor('red') // -> '#FF0000'
+ *
+ *
+ * @example <caption>Case method with caseValue as a function and correspondingValue as ordinary value:</caption>
+ *
+ * class Enemy {}
+ * const is = (prototype) => (value) => value instanceof prototype
+ *
+ * const greet = multi() // Uses identity function (x => x) as default dispatch
+ *
+ * // Matches, when case function executed with dispatch value returns truthy value
+ * const extendedGreet = method(is(Enemy), 'Goodbye!')(greet)
+ *
+ * extendedGreet(new Enemy()) // -> 'Goodbye!'
+ *
+ *
+ * @example <caption>Case method with caseValue and correspondingValue as functions:</caption>
+ *
+ * class Car { drive() { return 'driving...' } }
+ * class Human { walk() { return 'walking...' } }
+ * const is = (prototype) => (value) => value instanceof prototype
+ *
+ * const go = multi( // Uses identity function (x => x) as default dispatch
+ *   method(is(Car), (entity) => entity.drive()) // lets add one case to original multimethod
+ * )
+ *
+ * const extendedGo = method(is(Human), (entity) => entity.walk())(go)
+ *
+ * extendedGo(new Car()) // -> 'driving...'
+ * extendedGo(new Human()) // -> 'walking...'
+ *
+ *
+ * @see multi, fromMulti
+ */
+const method: Method = (caseValue, correspondingValue) => {
   const partialMethod = (multimethod) => {
     if (!multimethod[multimethodKey]) {
       throw new Error('Function is not a multimethod')
     }
 
-    const [first, second] = args
+    const first = caseValue
+    const second = correspondingValue
     const isNotDefault = second !== undefined
     const fn = isNotDefault ? second : first
     const dispatchValues = isNotDefault ? first : null

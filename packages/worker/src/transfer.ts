4e485c542dfed8ce72fc89fac749abb6e84aef8b
Maciej Cąderek <maciej.caderek@gmail.com>
2019-11-21 03:58:00 +0100

    Worker package - added JSDocs for the transfer function

--- packages/worker/src/transfer.ts
+++ packages/worker/src/transfer.ts
@@ -1,5 +1,14 @@
 import { Transfer, transferKey } from "./types"
 
+/**
+ * Wraps a worker handler so you can add a transfer list for the result.
+ *
+ * Works only if the result is an instance of a Buffer or a TypedArray.
+ *
+ * @param handler - Handler function
+ * @param mapperFn - Takes the result and should produce a transfer list.
+ * @returns A wrapped handler whose result will be moved rather than cloned.
+ */
 const transfer: Transfer = (handler, mapperFn) => (payload, workerData) => {
   const result = handler(payload, workerData)
   const transferList = mapperFn(result)

9d8cee0a45bf3028b975fe5fed22106a4202dbc0
Maciej Cąderek <maciej.caderek@gmail.com>
2019-12-08 16:05:39 +0100

    Arrows package - updated dependencies

--- packages/arrows/src/index.ts
+++ packages/arrows/src/index.ts
@@ -3,10 +3,9 @@ import com from "@arrows/composition"
 import dis from "@arrows/dispatch"
 import err from "@arrows/error"
 import mul from "@arrows/multimethod"
-import str from "@arrows/string"
 import wor from "@arrows/worker"
 
-export { arr, com, dis, err, mul, str, wor }
+export { arr, com, dis, err, mul, wor }
 
 export default {
   arr,
@@ -14,6 +13,5 @@ export default {
   dis,
   err,
   mul,
-  str,
   wor,
 }
